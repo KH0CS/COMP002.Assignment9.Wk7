dd4428b54c11859d95365b391c9f4e11b3016d83
Kam Rodriguez <kamrod1021@gmail.com>
2025-11-30 18:58:52 -0800

    I answered the prompted questions.

---
+++ script.js
@@ -0,0 +1,27 @@
+// 1. Write a console.log() statement that answers the following question: What worked
+// well for you over the last 9 weeks that aided in your learning process?
+
+console.log(
+  "What worked well for you over the last 9 weeks that aided in your learning process?"
+);
+console.log("Honestly watching and looking at videos/ or hearing people explain the concepts stuck with me more than just reading the book. the book is too bland/boring. I need better ways to get more involved and learn the material.I started freeCodeCamp and I downloaded apps on my phone to further myt learning.");
+
+// 2. Write a console.log() statement that answers the following question: What would you
+// change about the course?
+
+console.log("What would you change about the course?");
+console.log("I personally thought the inperson classes could cover more of the material. Understandably there is a shorter timeframe but it seems liek in class the material was gone over but not much help afterwards.");
+
+// 3. Write a console.log() statement that answers the following question: Did you learn
+// what you expected to learn in this course?
+
+console.log("Did you learn what you expected to learn in this course?");
+console.log("I learned that I need to take more time to understand the mateiral and certain concepts better. I prefer to make a game so someehting in that relm helps me learn better than just a class.");
+
+// 4. Write a console.log() statement that answers the following question: What concepts
+// were new to you and what concepts had you encountered already in the past?
+
+console.log(
+  "What concepts were new to you and what concepts had you encountered already in the past?"
+);
+console.log("Everything was pretty much new to me. There were some that I related to Godot and I have some experience messing with Godot so I would realize what the counterpart is and that made certain topics easier to grasp.");
